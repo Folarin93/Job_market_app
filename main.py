12b609ad5d34cdc5b83024f49b07eb630c65c3e1
Ubuntu <ubuntu@ip-172-31-80-189.ec2.internal>
2020-10-01 11:54:06 +0000

    language list updated

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from languages import list_of_languages
 from jobs import Job 
 from tabulate import tabulate
 from job_statistics import Stats
-
-list_of_languagesss = ['python', 'javascript', 'html', 'css', 'sql', 'java', 'rust', 'typescript']
     
 
 def presenter():
     table_info = []
-    for lang in list_of_languagesss:
+    for lang in list_of_languages:
         prog_lang = Stats(lang)
         """Table content creation for each programming lanaguage job market"""
         table_info.append(prog_lang.table_content)
